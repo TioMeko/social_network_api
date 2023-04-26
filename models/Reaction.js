e7ad8f118a56cf2b8f359ba3e71736828f466f37
NineTwoProgramming <marasimowicz@ymail.com>
2023-04-26 16:33:16 -0400

    updated reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,40 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createAt: {
+            type: Date,
+            default: Date.now,
+            get: function(date) {
+                return date.toLocaleString('en-US', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                  hour: 'numeric',
+                  minute: 'numeric',
+                  hour12: true
+                })}
+        },
+    },
+    {
+        toJSON: {
+          getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema;
